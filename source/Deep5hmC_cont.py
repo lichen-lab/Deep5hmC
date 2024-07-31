4404ac4cae0bccbd60e98bf90222778aad41d7f2
Xin Ma <136360597+XinBiostats@users.noreply.github.com>
2024-07-31 00:14:56 -0700

    Update Deep5hmC_cont.py

--- source/Deep5hmC_cont.py
+++ source/Deep5hmC_cont.py
@@ -5,21 +5,39 @@ from evaluator import *
 import json
 import argparse
 
+parser = argparse.ArgumentParser(description="Run model training and evaluation with config file")
+parser.add_argument('json_file', type=str, help='Path to the JSON configuration file')
+args = parser.parse_args()
 
-# load data for predicting continuous 5hmC read counts
-data_cont = MyDataset_cont(64*8,['H3K4me1','H3K4me3'])
+with open(args.json_file, 'r') as f:
+    config = json.load(f)
 
-# load Deep5hmC_cont
-model_cont = Deep5hmC_cont(input_read_size=data_cont.read_size)
+data_path = config['data_path']
+weights_path = config['weights_path']
+prediction_path = config['prediction_path']
 
+model_types = config['model_types']
+tissue = config['tissue']
+histones = config['histones']
 
-# train Deep5hmC_binary
-trainer = Trainer(num_epochs=200, lr=1e-3, earlystop_thresh=20)
+batch_size = config['batch_size']
+num_epochs = config['num_epochs']
+lr = config['lr']
+earlystop_thresh = config['earlystop_thresh']
 
-trainer.fit_cont(model_cont,data_cont)
+# load data for predicting continuous 5hmC peaks
+data = MyDataset(data_path,tissue,histones, batch_size)
 
+# load continuous
+model = Deep5hmC_cont(input_read_size=data.read_size)
 
-# evaluate Deep5hmC_binary
-evaluator = Evaluator(model_cont, '../parameters/Deep5hmC_cont.pth')
+# train Deep5hmC_cont
+trainer = Trainer(weights_path = weights_path, tissue = tissue, num_epochs=num_epochs, lr=lr, earlystop_thresh=earlystop_thresh)
 
-evaluator.eval_model_cont(data_cont,verbose=1)
+trainer.fit(model = model, data = data, model_type = model_types[1])
+
+# evaluate Deep5hmC_cont
+evaluator = Evaluator(prediction_path = prediction_path, tissue = tissue, model=model, best_model = trainer.best_model)
+# evaluator = Evaluator(prediction_path = prediction_path, tissue = tissue, model=model, best_model = f'../pretrained/Deep5hmC_cont.pth') # evaluate pretrained model
+
+evaluator.eval_model(data, model_type = model_types[1], verbose=1)
